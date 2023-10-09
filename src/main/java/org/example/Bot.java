ac335fe2ae58bdd69df4fe1bf6ae0a348cd71a75
stalien <stalien@yandex.ru>
2023-10-09 17:44:03 +0300

    minimal functionality

---
+++ src/main/java/org/example/Bot.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+public class Bot extends TelegramLongPollingBot {
+    @Override
+    public String getBotUsername() {
+        return null;
+    }
+
+    @Override
+    public String getBotToken() {
+        return null;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        System.out.println(update);
+    }
+}
